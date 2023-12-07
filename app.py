32cd464ea1251e4a92be6ee63a55050af788f54b
Amand <amandsingh1996@gmail.com>
2023-12-07 12:38:04 -0500

    app testing

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def hello():
     return "Learning DevOps with git"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5002))
     app.run(debug=True,host='0.0.0.0',port=port)
